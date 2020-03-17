b9fa9755dcb8f928b8028a4d75e9be144c3043fc
1138703080@qq.com <1138703080@qq.com>
2020-03-17 10:35:39 +0800

    基本css引入

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: your name
  * @Date: 2020-03-17 09:50:00
- * @LastEditTime: 2020-03-17 09:55:52
- * @LastEditors: your name
+ * @LastEditTime: 2020-03-17 10:29:24
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vueclic:\Users\Administrator\Desktop\vue\shopv\src\App.vue
  -->
@@ -21,12 +21,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/css/base.css";
 </style>
